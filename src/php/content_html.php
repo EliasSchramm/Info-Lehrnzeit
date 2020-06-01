5fd99054581d40ff781b5d31639ccfeeb79e76fb
EPS-DEV <49179221+EliasSchramm@users.noreply.github.com>
2020-06-01 18:52:05 +0200

    Update

--- src/php/content_html.php
+++ src/php/content_html.php
@@ -1,2 +1,4 @@
 <h2>Kurze Einleitung</h2>
 <p>HTML ist die Sprache welche <u>beschreibt</u>, welche Elemente mit welchem Inhalt sich auf der Website befinden.</p>
+<p>Mit dieser Sprache ist es möglich die Website zu strukturieren (Text, Links, Formulare, Bilder, ... einfügen), sowie Text grob zu formatieren.</p>
+<p>Style erhält die Website mit CSS und Funktion erhält die Website durch JavaScript.</p>
--- src/php/header.php
+++ src/php/header.php
@@ -15,7 +15,7 @@
       <div class="language_category" id="css">CSS</div>
       <div class="language_category" id="js">JavaScript</div>
       <div class="language_category" id="php">PHP</div>
-
+      <div></div>
 
 
     </header>
