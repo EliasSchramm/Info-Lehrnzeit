3aedfbfc60136e48cf225b82f66343d9dab02f02
EPS-DEV <49179221+EliasSchramm@users.noreply.github.com>
2020-06-01 23:19:05 +0200

    Update

--- src/php/content_html.php
+++ src/php/content_html.php
@@ -1,4 +1,6 @@
 <h2>Kurze Einleitung</h2>
-<p>HTML ist die Sprache welche <u>beschreibt</u>, welche Elemente mit welchem Inhalt sich auf der Website befinden.</p>
-<p>Mit dieser Sprache ist es möglich die Website zu strukturieren (Text, Links, Formulare, Bilder, ... einfügen), sowie Text grob zu formatieren.</p>
-<p>Style erhält die Website mit CSS und Funktion erhält die Website durch JavaScript.</p>
+<p class="start_text">HTML ist die Sprache welche <u>beschreibt</u>, welche Elemente mit welchem Inhalt sich auf der Website befinden.<br>
+Mit dieser Sprache ist es möglich die Website zu strukturieren (Text, Links, Formulare, Bilder, ... einfügen), sowie Text grob zu formatieren.<br>
+Style erhält die Website mit CSS und Funktion erhält die Website durch JavaScript.</p>
+<p class="start_text">Im folgenden werde ich den Aufbau einer simplen Website erklären. Anschliesend werde ich ihr etwas Style(CSS) und Funktion(JavaScript) geben.
+Ich empfehle die Kategorien chronologisch durchzugehen.</p>
