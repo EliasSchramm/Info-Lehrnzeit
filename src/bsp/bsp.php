ea0735bf3dfacf4ceef27f4eecf40306d4bdf7c4
EPS-DEV <49179221+EliasSchramm@users.noreply.github.com>
2020-06-04 00:03:59 +0200

    Update

--- src/bsp/bsp.php
+++ src/bsp/bsp.php
@@ -4,15 +4,62 @@
 
   <head>
     <meta charset="utf-8"/>
+    <link rel="stylesheet" href="css/bsp<?php echo $_GET["style"]; ?>.css" type="text/css"/>
     <title>Lorem ipsum</title>
   </head>
   <body>
 
-    <?php
+    <div class="kopf">
 
-    printf($_GET["style"]);
+      <h1>Überschrift 1</h1>
 
-    ?>
+      <h2>Überschrift 2</h2>
+
+      <h3>Überschrift 3</h3>
+
+      <p class="einleitung">Hallo, ich bin ein sehr, sehr sinvoller <span class="rot"> Einleitungstext. </span> </p>
+
+    </div>
+
+    <div class="inhalt">
+      <hr>
+      <p>Das is n tolles Bild:</p>
+
+      <img src="img/bild.jpg" alt="bild">
+
+      <p>"Lorem ipsum" ist ein Fülltext der es ermöglicht die Website zu gestalten selbst wenn es noch keinen fertigen Inhalt gibt.</p>
+
+      <p class="grün_hervorgehoben">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+
+      <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+
+      <p>Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. </p>
+
+      <p>Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer</p>
+
+      <p><a href="https://www.loremipsum.de/">Hier weiterlesen</a><p>
+
+        <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+
+        <li>
+          Items:
+          <ul>
+              <li>Item 4</li>
+              <li>Item 5</li>
+              <li>Item 6</li>
+          </ul>
+        </li>
+
+      </ul>
+    </div>
+
+    <div class="fuss">
+      <hr>
+      <p>Sponsored by to much coffe</p>
+    </div>
 
   </body>
 </html>
