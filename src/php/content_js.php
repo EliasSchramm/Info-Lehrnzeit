40cea050aa61312a215c977e035a834e1ec8de54
EPS-DEV <49179221+EliasSchramm@users.noreply.github.com>
2020-06-10 16:42:11 +0200

    Update

--- src/php/content_js.php
+++ src/php/content_js.php
@@ -1,1 +1,5 @@
-js
+<h2>Kurze Einleitung</h2>
+<p class="start_text">
+  JavaScript (JS) ist eine Sprache welche einer Website Funktion geben kann. Es ist zum Beispiel möglich Dinge auszuführen wenn der Nutzer auf ein Element gedrückt hat oder
+  den Inhalt eines Elementes zu ändern. Im folgenden werde ich ein paar Grundlagen erklären und damit am Ende ein kleines Tic-Tac-Toe programmieren. 
+</p>
