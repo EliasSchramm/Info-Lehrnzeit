81ac2d743703229811cae40fb0a4f6fd10b802b9
EliasSchramm <privat@eps-dev.de>
2020-08-23 21:55:00 +0200

    Update

--- src/php/footer.php
+++ src/php/footer.php
@@ -1,3 +1,10 @@
+    <section>
+    <div class="wave wave1"></div>
+    <div class="wave wave2"></div>
+    <div class="wave wave3"></div>
+    <div class="wave wave4"></div>
+    </section>
+
     <script src="js/w3highlighter.js"></script>
     <script src="js/header_hover.js"></script>
   </body>
